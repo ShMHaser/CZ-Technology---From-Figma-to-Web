749f85701a3c011d12c81e1ffd5e1874a7235b70
ShMHaser <s.m.hindhol@northsouth.edu>
2024-04-25 01:50:37 +0600

    Bug fixing and adding the slider.

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   $(".sliders").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
+    arrows: true,
+    dots: true,
+    prevarrow: '<i class="fa-solid fa-circle-left"></i>',
+    nextarrow: '<i class="fa-solid fa-circle-right"></i>',
+    autoplay: true,
   });
 });
